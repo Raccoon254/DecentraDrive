0480c06d9e013260cfbc2a339303542415715e65
Raccoon254 <tomsteve187@gmail.com>
2024-10-04 20:14:10 +0300

    Added tailwind and daisy UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Decentralized Drive</h1>
+      <h1 className='text-blue-600'>Decentralized Drive</h1>
       <FileUpload />
       <FileList />
     </div>
